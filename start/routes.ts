4cdfbb7c16772fdcc9e9215223b592021031cdd0
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-10-01 17:45:43 +0200

    chore: fix ESLint

--- start/routes.ts
+++ start/routes.ts
@@ -12,20 +12,18 @@ import router from '@adonisjs/core/services/router'
 const HoursController = () => import('#controllers/hours_controller')
 const WorkersController = () => import('#controllers/workers_controller')
 
-router.group(() => {
-  router.group(() => {
-    router.resource('hours', HoursController)
-      .apiOnly()
-      .params({
-        hours: 'id'
+router
+  .group(() => {
+    router.group(() => {
+      router.resource('hours', HoursController).apiOnly().params({
+        hours: 'id',
       })
-  })
-  
-  router.group(() => {
-    router.resource('workers', WorkersController)
-      .apiOnly()
-      .params({
-        workers: 'id'
+    })
+
+    router.group(() => {
+      router.resource('workers', WorkersController).apiOnly().params({
+        workers: 'id',
       })
+    })
   })
-}).prefix('api/v1')
+  .prefix('api/v1')
