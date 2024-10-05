097da8495826c2437f5452e73f485535ea02427c
Jakub Czajkowski <and.jj.an@gmail.com>
2024-10-06 01:48:02 +0200

    feat: add W3 scraping script

---
+++ scripts/workers/w3.ts
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+import Worker from '#models/worker'
+
+export async function scrapeWorkersW3() {
+  const facultyNumber = 3
+  const url = 'https://wch.pwr.edu.pl/studenci/dziekanat'
+
+  const phoneRegex = /tel\.\s*([+\d\s]+)/
+  const emailRegex = /([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/
+
+  const response = await axios.get(url)
+  const $ = cheerio.load(response.data)
+  const workers: Partial<Worker>[] = []
+
+  // office leader
+  const leaderInfo = $(
+    '#module-description-content > div:nth-child(2) > div > div > div > div > div > p:nth-child(5)'
+  )
+    .html()
+    ?.split('<br>')
+  const leaderName = leaderInfo ? leaderInfo[1] : ''
+  const leaderEmail = leaderInfo && leaderInfo[2] ? leaderInfo[2].match(emailRegex) : undefined
+  const cleanedLeaderEmail = leaderEmail ? leaderEmail[0] : undefined
+  const leaderPhoneNumber =
+    leaderInfo && leaderInfo[3] ? leaderInfo[3].match(phoneRegex) : undefined
+  const cleanedLeaderPhoneNumber = leaderPhoneNumber ? leaderPhoneNumber[1] : undefined
+
+  workers.push({
+    name: leaderName,
+    phoneNumber: cleanedLeaderPhoneNumber,
+    email: cleanedLeaderEmail,
+    deansOfficeId: facultyNumber,
+  })
+
+  for (const elem of $(
+    '#module-description-content > div:nth-child(2) > div > div > div > div > div > ul:nth-child(7) > li'
+  )) {
+    const name = $(elem).contents().first().text().trim()
+    const cleanedName = name.replace(/\s*\(.*?\)\s*/g, ' ').trim()
+
+    const phoneMatch = $(elem).html()?.match(phoneRegex)
+    const phoneNumber = phoneMatch ? phoneMatch[1].trim() : undefined // Get the first capturing group
+
+    const email = $(elem).find('a').text().trim() // Get the text of the anchor tag
+    workers.push({
+      name: capitalize(cleanedName),
+      phoneNumber,
+      email,
+      deansOfficeId: facultyNumber,
+    })
+  }
+
+  return workers
+}
+
+function capitalize(str: string): string {
+  return str
+    .toLowerCase() // Convert the entire string to lowercase first
+    .split(/[\s]+/) // Split the string into words on spaces
+    .map((word) => {
+      // Check if the word contains a hyphen
+      if (word.includes('-')) {
+        return word
+          .split('-') // Split on hyphen
+          .map((part) => part.charAt(0).toUpperCase() + part.slice(1)) // Capitalize each part
+          .join('-') // Join the parts back with hyphen
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1) // Capitalize regular words
+    })
+    .join(' ') // Join the words back into a string
+}
